5158867584bc14db3100d70b71325b20af543312
AltESV <ishigami.daniel@gmail.com>
2024-04-16 09:36:18 +0100

    change to es6

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
-const tmi = require('tmi.js');
-require('dotenv').config();
-const OpenAI = require('openai');
+import tmi from 'tmi.js';
+import dotenv from 'dotenv';
+dotenv.config();
+import { OpenAI } from 'openai';
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
