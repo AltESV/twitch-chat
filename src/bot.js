1bdc8b2fdf4fdfe06eb9e860154e439f09218ae8
AltESV <ishigami.daniel@gmail.com>
2024-04-19 10:36:42 +0100

    refactor contd

--- src/bot.js
+++ src/bot.js
@@ -1,29 +1,23 @@
 import tmi from 'tmi.js';
-import dotenv from 'dotenv';
-dotenv.config();
+import { config } from './config/config.js';
 import axios from 'axios';  
-import { OpenAI } from 'openai';
+import { generate } from './api/openai.js';
 import { createClient } from '@supabase/supabase-js';
 
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+const supabase = createClient(config.supabaseUrl, config.supabaseKey);
 
-
-//AUTH 
 let client;
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REFRESH_URL = 'https://id.twitch.tv/oauth2/token';
 
+//token refresh handler
 async function refreshAccessToken(refreshToken) {
     const params = new URLSearchParams();
     params.append('grant_type', 'refresh_token');
     params.append('refresh_token', refreshToken);
-    params.append('client_id', CLIENT_ID);
-    params.append('client_secret', CLIENT_SECRET);
+    params.append('client_id', config.clientId);
+    params.append('client_secret', config.clientSecret);
 
     try {
-        const response = await axios.post(REFRESH_URL, params);
+        const response = await axios.post(config.refreshUrl, params);
         const { access_token, expires_in, refresh_token } = response.data;
         console.log(`New Access Token: ${access_token} which expires in ${expires_in} seconds`);
         return { accessToken: access_token, expiresIn: expires_in, refreshToken: refresh_token };
@@ -33,20 +27,20 @@ async function refreshAccessToken(refreshToken) {
     }
 }
 
-refreshAccessToken(process.env.TWITCH_REFRESH_TOKEN).then(tokens => {
+refreshAccessToken(config.twitchRefreshToken).then(tokens => {
     if (tokens) {
-        process.env.TWITCH_ACCESS_TOKEN = tokens.accessToken;
-        process.env.TWITCH_REFRESH_TOKEN = tokens.refreshToken;
+        config.twitchAccessToken = tokens.accessToken;
+        config.twitchRefreshToken = tokens.refreshToken;
         initializeClient(tokens.accessToken);  
     }
 });
 
 setInterval(() => {
-    refreshAccessToken(process.env.TWITCH_REFRESH_TOKEN).then(tokens => {
+    refreshAccessToken(confi.twitchRefreshToken).then(tokens => {
         if (tokens) {
             console.log('Automatically refreshed Access Token.');
-            process.env.TWITCH_ACCESS_TOKEN = tokens.accessToken;
-            process.env.TWITCH_REFRESH_TOKEN = tokens.refreshToken;
+            config.twitchAccessToken = tokens.accessToken;
+            config.twitchRefreshToken = tokens.refreshToken;
             initializeClient(tokens.accessToken);  
         }
     });
@@ -56,14 +50,14 @@ function initializeClient(token, channels) {
     const opts = {
         options: { debug: true },
         identity: {
-            username: process.env.BOT_USERNAME,
+            username: config.botUserName,
             password: `oauth:${token}`
         },
         channels  
     };
 
 
-//CHAT
+//client
     client = new tmi.Client(opts);
 
     client.on('message', onMessageHandler);
@@ -78,31 +72,6 @@ function initializeClient(token, channels) {
     client.connect().catch(console.error);
 }
 
-async function generate(message) {
-    try {
-        const faqText = `FAQ:
-        - What is Fana? Fana is the charity of charities! We bring vetted and high impact projects to Twitch streamers and their communities.
-        - What charity is being supported? In April in support of Earth Day, Fana is supporting projects providing clean water in Ethiopia, Madagascar, and Sierra Leone.
-        - What's the goal? The goal is to raise $1000 for the project.
-        - Why Fana and not another charity? We provide insight into where your money actually goes, provide regular content about your supported projects, and rotate through different projects each month!
-        - Is this charity registered and tax-exempt? Yes! Fana Impact Foundation, Inc. is a registered 501(c)(3) non-profit in the United States.
-        - Are there any other ways to support the charity or Fana other than through money? You can help us find other streamers who want to join our creators for good!
-        - Will there be transparency on how much was raised today? Absolutely, we'll provide the streamer with a recap of total amounts raised and what it went towards a few days after the stream!
-        - What are your costs or fees? We are a US registered charity and operate with transparency! Our directors and employees are all volunteers and are unpaid.  We do have a technology fee of approximately 10% of our revenues that go to Fana Technologies who provides financial, operational, and technology services for us.  We then have standard operating expenses such as marketing costs and donation processing costs from PayPal and Stripe.`;
-
-        const completion = await openai.chat.completions.create({
-            messages: [{
-                role: "system", content:
-                    `You are the fana twitch chatbot that provides information exclusively about https://impact.fanaverse.io/. You also use ${faqText} to answer questions.`
-            }, { role: "user", content: message }],
-            model: "gpt-3.5-turbo",
-        });
-        return completion.choices[0].message.content.trim();
-    } catch (error) {
-        console.error("OpenAI Error:", error);
-        return "Error generating response. Please try again.";
-    }
-}
 
 async function onMessageHandler(channel, tags, msg, self) {
     if (self) return;
@@ -169,7 +138,7 @@ async function getActiveChannels() {
 
 getActiveChannels().then(channels => {
     if (channels.length > 0) {
-        initializeClient(process.env.TWITCH_ACCESS_TOKEN, channels);
+        initializeClient(config.twitchAccessToken, channels);
     } else {
         console.log('No active channels found. Ensure your database is populated.');
     }
